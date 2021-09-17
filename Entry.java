a46f09d8bf984dbdf3d71882d55f59458dc1a61b
Tushar2816263 <65064322+Tushar2816263@users.noreply.github.com>
2021-09-17 12:24:24 +0530

    Create Entry.java

---
+++ Entry.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter Value Of a");
+		int a = scn.nextInt();
+		System.out.println("Enter Value Of b");
+		int b=  scn.nextInt();
+		int c;
+		
+		
+		
+		c = a + b;
+		System.out.println("Result is "+c);
+	}
+
+}
+
+
+//class Calculator
+//{
+//	
+//}
